be4c716731c0b10a7591b142e43e2c0ee9653a59
narekhuneyan-crypto <narek.huneyan@tumo.org>
2026-07-11 13:39:19 +0400

    first commit

---
+++ audio.js
@@ -0,0 +1,27 @@
+var data = {
+    title : [
+    "music/Armani White - BILLIE EILISH.",
+    "music/Tatoul Avoyan (Tatul) - Srti banali.",
+    "c/Shakira_-_Waka_Waka_Original.",
+    "music/Mamba_-_Otido_(www.mexedi.am)",
+    "music/alice_soundz-best-sword-sounds-slice-slash-crash-swoosh-etc-224090.",
+    ],
+
+   
+
+ song: [
+    "music/Armani White - BILLIE EILISH.mp3",
+    "music/Tatoul Avoyan (Tatul) - Srti banali.",
+    "c/Shakira_-_Waka_Waka_Original_(mp3.pm).",
+    "music/Mamba_-_Otido_(www.mexedi.am)",
+    "music/alice_soundz-best-sword-sounds-slice-slash-crash-swoosh-etc-224090."
+],
+poster : [
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMH-dyqXSzuuqJWekEfEQ72CJetXvPxfv7kdIOUJp5zg&s=10",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaZiTgEa9yt0gg73sfRjiE9EdrE8n3X0M7IotPkEdvA&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAx-aO5Dtb6EFhqqPXVb8tBNUN_S7i7AY8wU4mxJuC6g&s=10",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw7G3N6yRJiO9wDknMWaQOwhh6xlnHm35New9pQhjNXg&s=10",
+"https://cdn.mos.cms.futurecdn.net/8fNgFeVnhfSi3RxE5aHMLc.jpg",
+
+]
+}
